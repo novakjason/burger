3553ca7e157ac1dc9c1f2e6f118f947d395bd96a
Red Tail <redtail814@gmail.com>
2019-08-24 19:01:35 -0400

    Set up initial sequelize ORM and dotenv files.

---
+++ models/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int AUTO_INCREMENT NOT NULL,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ models/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ("Reese's Cup");
+INSERT INTO burgers (burger_name) VALUES ("It's a Cluckin' Burger");
+INSERT INTO burgers (burger_name) VALUES ('The Schwarzenegger');
+INSERT INTO burgers (burger_name) VALUES ('Norris Chuck');
+INSERT INTO burgers (burger_name) VALUES ('Veg Out');
+INSERT INTO burgers (burger_name) VALUES ('Crybaby');
