eca136615d2274c2f9edfa891d2009bd547b8b07
Red Tail <redtail814@gmail.com>
2019-08-26 09:30:18 -0400

    Organized server code: configured express, added routes, synced models

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
+// dependencies
 require('dotenv').config();
 const express = require("express");
+
+// sets up express and port
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// required models for syncing
 const db = require("./models");
 
+// express data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
-const app = express();
+// static directory
+app.use(express.static("public"));
 
-db.sequelize.sync().then(() => {
+// routes
+require("./routes/htmlRoutes.js")(app);
+require("./routes/apiRoutes.js")(app);
+
+// syncing sequelize models and then starting express app
+db.sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => {
         console.log("Listening!  Access http://localhost:3000");
     });
