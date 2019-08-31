52da2a2094d401b3dd7dc9c6f09c88e9e67a67ad
Red Tail <redtail814@gmail.com>
2019-08-31 01:05:12 -0400

    Removed where parameter from get API route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,19 +5,15 @@ module.exports = function (app) {
   // gets all burgers
   app.get("/api/burgers", function (req, res) {
 
-    db.Burger.findAll({
-      where: query
-    }).then(function (dbBurgers) {
+    db.Burger.findAll({}).then(function (dbBurgers) {
       res.json(dbBurgers);
     });
-
   });
 
   // create a new burger
   app.post("/api/burgers", (req, res) => {
     db.Burger.create(req.body).then(dbBurgers => {
-      res.json(dbBurgers);
+        res.json(dbBurgers);
     });
   });
-
 };
