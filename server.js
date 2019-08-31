b09814b2a92d9f4a37ca8fcd26c793b4fe5a70fd
Red Tail <redtail814@gmail.com>
2019-08-31 06:03:43 -0400

    Added test options

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 // dependencies
-require('dotenv').config();
-const express = require("express");
-const exphbs = require("express-handlebars");
-
-// sets up express and port
-const app = express();
-const PORT = process.env.PORT || 3000;
+require("dotenv").config();
+var express = require("express");
+var exphbs = require("express-handlebars");
 
 // required models for syncing
-const db = require("./models");
+var db = require("./models");
 
-// express data parsing
-app.use(express.urlencoded({ extended: true }));
+// sets up express and port
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// express middleware
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // static directory
@@ -19,26 +19,33 @@ app.use(express.static("public"));
 
 // handlebars engine
 app.engine(
-    "handlebars",
-    exphbs({
-      defaultLayout: "main"
-    })
-  );
-  app.set("view engine", "handlebars");
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
 
 // routes
-require("./routes/htmlRoutes.js")(app);
-require("./routes/apiRoutes.js")(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-// syncing sequelize models and then starting express app
-db.sequelize.sync({ force: true }).then(() => {
-    app.listen(PORT, () => {
-      console.log(
-        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-        PORT,
-        PORT
+var syncOptions = { force: false };
+
+// for test, set syncOptions.force to true to clearing the `testdb`
+if (process.env.NODE_ENV === "test") {
+  syncOptions.force = true;
+}
+
+// Starting the server, syncing our models
+db.sequelize.sync(syncOptions).then(function() {
+  app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
     );
-    });
+  });
 });
 
 module.exports = app;
