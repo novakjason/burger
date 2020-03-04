ee9c56cf7413363fc9f88a0fb735fe03eb52bc3d
Jason Novak <novak.jason7@gmail.com>
2020-03-03 22:32:06 -0500

    Updated table name in seeds.sql

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO burgers (burgerName, devoured) VALUES ("Reese's Cup", true);
-INSERT INTO burgers (burgerName, devoured) VALUES ("It's a Cluckin' Burger", false);
-INSERT INTO burgers (burgerName, devoured) VALUES ('Veg Out', true);
-INSERT INTO burgers (burgerName, devoured) VALUES ('Crybaby', true);
+INSERT INTO Burgers (burgerName, devoured) VALUES ("Reese's Cup", true);
+INSERT INTO Burgers (burgerName, devoured) VALUES ("It's a Cluckin' Burger", false);
+INSERT INTO Burgers (burgerName, devoured) VALUES ('Veg Out', true);
+INSERT INTO Burgers (burgerName, devoured) VALUES ('Crybaby', false);
